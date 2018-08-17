2aa92305d911980f26b51d71dbad45914b815fd3
Cozmanca Alexandru <alexandru.cozmanca@gmail.com>
2018-08-17 12:23:16 +0300

    third commit

--- Algorithms/src/wandering_salesman/WanderingSalesmanTest.java
+++ Algorithms/src/wandering_salesman/WanderingSalesmanTest.java
@@ -3,6 +3,9 @@ package wandering_salesman;
 import java.util.Arrays;
 
 // always chose the closest town as next destination as long as the town has not been previously visited
+// for ease of comprehension algorithm works from 0 upwards, but input/output is from 1 upwards
+// (town 1 will be treaded in the algorithm as index 0 and so forth
+
 class HeuristicWanderingSalesman
 {
 	int[][] distances;
@@ -22,40 +25,50 @@ class HeuristicWanderingSalesman
 	{
 		int visitedTowns = 1;
 		int destinationCandidate;
+		
+		// prime while loop and arbitrarily choose the new candidate as 0 if the departure town is not the first town or 1 otherwise
 		if (startingNode == 0)
 			destinationCandidate = 1;
 		else 
 			destinationCandidate = 0;
 				
+		
 		while (visitedTowns < distances.length)
 		{			
-															
+											
+			// find the closest unvisited town
 			for (int i = 0; i < distances.length; i++)
 			{
-																		
-				System.out.println(distances[path[visitedTowns-1]][destinationCandidate]);
-				System.out.println(distances[path[visitedTowns-1]][i]);
-				System.out.println();
+				//display comparisons														
+				//System.out.println(distances[path[visitedTowns-1]][destinationCandidate]);
+				//System.out.println(distances[path[visitedTowns-1]][i]);
+				//System.out.println();
 				
+				// check if the closest available town has been visited before and eliminate same-town minimum value
 				if (distances[path[visitedTowns - 1]][destinationCandidate] >= distances[path[visitedTowns -1 ]][i] &&  
 						distances[path[visitedTowns - 1]][i]!=0 && i != startingNode)
 				{ 
+					// store current candidate in case it is not valid
 					int previousCandidate = destinationCandidate;
 					destinationCandidate = i;
 					
 					for (int j = visitedTowns; j >= 0; j--)
 						if(path[j] == i)
 						{
+							// reset and break loop;
 							destinationCandidate = previousCandidate;
 							break;
 						}
 				}
 			}
-					
+			
+			// add candidate to path and increase visitedTowns counter
 			path[visitedTowns] = destinationCandidate;
 			visitedTowns ++;
-			System.out.println(Arrays.toString(path));
-			System.out.println();
+			
+			// intermediate visual aid
+			//System.out.println(Arrays.toString(path));
+			//System.out.println();
 			
 			
 			// propose next candidate by finding the first unvisited town
@@ -63,9 +76,6 @@ class HeuristicWanderingSalesman
 			for(int i = 0; i < visitedTowns; i ++)
 				if (destinationCandidate >= path[i])
 					destinationCandidate = i + 1;
-						
-			
-						
 		}
 		
 		return path;
